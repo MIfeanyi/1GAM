1720f1a4290242294294cebd339baa8d4f87f02b
Michael Redford <michaeliredford@gmail.com>
2017-04-09 18:03:57 +0100

    added support for changing text size

--- 2017/Apr/Movie/dialog.lua
+++ 2017/Apr/Movie/dialog.lua
@@ -1,12 +1,16 @@
 dialog = {
     fontSize=12,
     fontColor ={r=255,g=255,b=255},
+    windowColor ={r=0,g=0,b=255},
     nextChar = 1,
     curT = 0,
-    waitT = 1.05,
+    waitT = 0.2,
     speed = 3,
     dialogs = {}, --text={id,string,imgId,}
-    finished = false
+    finished = false,
+    height=200,
+    window="default", -- draw via colors
+    border = 10
 }
 
 function dialog:changeColor(r,g,b)
@@ -14,8 +18,12 @@ function dialog:changeColor(r,g,b)
     color.r, color.g, color.b = r,g,b
 end
 
-function dialog:push(id,text,imgID)
+function dialog:push(id,text,imgID,size)
     newDialog = {id=id,text=text,imgID=imgID}
+    if size ~= nil then
+        --love.graphics.setNewFont(size)
+        newDialog.size = size
+    end
     table.insert(self.dialogs,newDialog)
 end
 
@@ -24,6 +32,9 @@ function dialog:update(dt)
     if self.curT > self.waitT then
         self.nextChar = self.nextChar+self.speed
         self.curT=0
+    end
+    if self.dialogs[1].size ~= nil then
+        love.graphics.setNewFont(self.dialogs[1].size)
     end
     if string.len(self.dialogs[1].text) <= self.nextChar then
         self.nextChar = string.len(self.dialogs[1].text)
@@ -37,8 +48,12 @@ function dialog:update(dt)
 end
 
 function dialog:draw()
+    windowColor = self.windowColor
+    love.graphics.setColor(windowColor.r,windowColor.g,windowColor.b,255)
+    love.graphics.rectangle("fill", 0, 0,love.graphics.getWidth(),self.height)
+    love.graphics.setColor(255,255,255,255)
     text = self.dialogs[1].text
-    love.graphics.printf(string.sub(text,0,self.nextChar),0,0,200,"left")
+    love.graphics.printf(string.sub(text,0,self.nextChar),self.border,self.border,love.graphics.getWidth()-self.border,"left")
 end
 
 function dialog:reset()
