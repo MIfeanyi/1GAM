c6370d8231941e4cbff18a6e25d7e84e67fab93f
Michael Redford <michaeliredford@gmail.com>
2017-04-22 15:35:35 +0100

    rewrote sound module

--- 2017/Apr/Movie/sound.lua
+++ 2017/Apr/Movie/sound.lua
@@ -1,24 +1,47 @@
 
-sounds = {}
+sound = {
+    sounds = {}, -- stored sounds
+    queue = {} -- queue for sound actions
+}
 
-function addSound(src,id)
+function sound:isEmpty()
+    for _, _ in ipairs(self.queue) do
+        return false
+    end
+    return true
+end
+
+function sound:addSound(src,id)
     local sound = love.audio.newSource(src,"static")
     local newSound = {sfx = sound, id=id}
-    table.insert(sounds,newSound)
+    table.insert(self.sounds,newSound)
 end
 
-function addMusic(src,id)
+function sound:addMusic(src,id)
     local sound = love.audio.newSource(src)
     local newSound = {sfx = sound, id=id}
-    table.insert(sounds,newSound)
+    table.insert(self.sounds,newSound)
 end
 
-function getSound(id)
-    for i, s in ipairs(sounds) do
+function sound:getSound(id)
+    for i, s in ipairs(self.sounds) do
         if s.id == id then
             return s.sfx
         end
     end
+end
+
+function sound:push(id,action)
+    newSound = {id=id,action=action}
+    table.insert(self.sounds,newSound)
+end
+
+function sound:update(dt)
+    if self:isEmpty() == false then
+        if self.queue[1].action == "play" then
+            love.audio.play(self:getSound(self.queue[1].id))
+        elseif self.queue[1].action == "stop" then
+            love.audio.stop(self:getSound(self.queue[1].id))
 end
 
 function deleteSound(id)
